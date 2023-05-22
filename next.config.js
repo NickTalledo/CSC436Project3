2f092ac323fdd453b8b31f6ecf226589565bb6e9
Nick Talledo <nicolastalledo13@gmail.com>
2023-05-22 16:39:41 -0400

    next-config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
   images: {
     domains: ["placebear.com"],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        encoding: false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
